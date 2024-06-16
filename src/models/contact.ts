5a98bc9a59ce855040f613fa5a4719cd5c3910cf
MPCGomes <mpcgomes2@gmail.com>
2024-06-16 15:34:27 -0300

    Contact Model Identation Fix

--- src/models/contact.ts
+++ src/models/contact.ts
@@ -1,16 +1,36 @@
 export interface Phone {
   number: string;
-  label: "None" | "Mobile" | "Work" | "Home" | "Main" | "Work Fax" | "Home Fax" | "Pager" | "Other" | "Custom";
+  label:
+    | "None"
+    | "Mobile"
+    | "Work"
+    | "Home"
+    | "Main"
+    | "Work Fax"
+    | "Home Fax"
+    | "Pager"
+    | "Other"
+    | "Custom";
 }
 
 export interface Email {
   address: string;
-  label: "None" | "Home" | "Work" | "Other" | "Custom";
+  label:
+    | "None"
+    | "Home"
+    | "Work"
+    | "Other"
+    | "Custom";
 }
 
 export interface SignificantDate {
   date: string;
-  label: "None" | "Birthday" | "Anniversary" | "Other" | "Custom";
+  label:
+    | "None"
+    | "Birthday"
+    | "Anniversary"
+    | "Other"
+    | "Custom";
 }
 
 export interface Contact {
