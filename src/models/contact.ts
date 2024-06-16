a0d2534f4dabdf25a593a63ea5be2ed6f16fc27d
MPCGomes <mpcgomes2@gmail.com>
2024-06-16 15:10:57 -0300

    Contact Model

---
+++ src/models/contact.ts
@@ -0,0 +1,25 @@
+export interface Phone {
+  number: string;
+  label: "None" | "Mobile" | "Work" | "Home" | "Main" | "Work Fax" | "Home Fax" | "Pager" | "Other" | "Custom";
+}
+
+export interface Email {
+  address: string;
+  label: "None" | "Home" | "Work" | "Other" | "Custom";
+}
+
+export interface SignificantDate {
+  date: string;
+  label: "None" | "Birthday" | "Anniversary" | "Other" | "Custom";
+}
+
+export interface Contact {
+  id: string;
+  picture?: string;
+  firstName: string;
+  lastName?: string;
+  company?: string;
+  phone: Phone[];
+  email?: Email[];
+  significanteDate?: SignificantDate[];
+}
