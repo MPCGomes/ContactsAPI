436d362c6333048a31d6bb05bfde42e98e2f8636
MPCGomes <mpcgomes2@gmail.com>
2024-06-16 15:36:00 -0300

    Contact Router

---
+++ src/routes/contactRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  getContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+} from "../controllers/contactController";
+
+const router = Router();
+
+router.get("/", getContacts);
+router.get("/:contactId", getContactById);
+router.post("/", createContact);
+router.put("/:contactId", updateContact);
+router.delete("/:contactId", deleteContact);
+
+export default router;
