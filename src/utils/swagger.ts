86e3cea3315dd778d49ff6d0b797d70174ecff9e
MPCGomes <mpcgomes2@gmail.com>
2024-06-16 15:42:18 -0300

    Swagger

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -3,3 +3,8 @@ import yaml from "yamljs";
 import path from "path";
 import { Express } from "express";
 
+const swaggerDocument = yaml.load("./swagger.yaml");
+
+export const setupSwagger = (app: Express) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+};
